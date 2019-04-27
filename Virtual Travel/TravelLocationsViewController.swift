01b53f894eb89f9d1d37ee66d6ea90d10f82ca08
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-04-27 12:54:45 -0300

    Create project

---
+++ Virtual Travel/TravelLocationsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TravelLocationsViewController.swift
+//  Virtual Travel
+//
+//  Created by Tiago Oliveira on 27/04/19.
+//  Copyright © 2019 Tiago Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
